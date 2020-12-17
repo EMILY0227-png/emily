bbe942438e7577fd495597e2ece908451de499f4
emily <10935021@dcsh.tp.edu.tw>
2020-12-17 12:05:58 +0800

    刪除

---
+++ guestbook/web/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from .models import Message
+
+# Create your views here.
+class MessageList(ListView):
+    model = Message
+
+class MessageDetail(DetailView):
+    model = Message
+
+class MessageCreate(CreateView):
+    model = Message
+    fields = '__all__'
+    success_url = reverse_lazy('msg_list')
+    
+class MessageDelete(DeleteView):
+    model = Message
+    success_url = reverse_lazy('msg_list')
