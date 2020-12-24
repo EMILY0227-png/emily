2d86ad0f5ec293662fb41c67b97d6aa85571569f
emily <10935021@dcsh.tp.edu.tw>
2020-12-24 11:25:26 +0800

    刪除完成,登出畫面調整

--- guestbook/web/views.py
+++ guestbook/web/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Message
+
 
 # Create your views here.
 class MessageList(ListView):
@@ -18,3 +19,5 @@ class MessageCreate(CreateView):
 class MessageDelete(DeleteView):
     model = Message
     success_url = reverse_lazy('msg_list')
+
+
